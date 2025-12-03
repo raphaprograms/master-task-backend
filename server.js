b87234d6c853564e758b574b633521e032b6b9dd
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-12-03 13:45:31 -0800

    Initialize project

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+// Set up all variables in the .env file
+require('dotenv').config();
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Successfully Connected to MongoDB!'))
+.catch(err => console.error('Connection error', err));
+
+const PORT = process.env.PORT || 4000;
+const app = express()
+
+
+// ====== Middleware ======
+app.use(morgan('dev')); // logger
+app.use(express.json()); // body parser
+require('./config/passport');
+
+// ==== Routes =============
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Use this route to set up the API documentation
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
